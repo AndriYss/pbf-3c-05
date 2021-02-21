64fe614f99c9fa372773dbea35b73090a2283959
AndriYss <andriyoga89@gmail.com>
2021-02-21 17:06:02 +0700

    PBF Praktikum 1 dan 2
    PBF Praktikum 1 dan 2

---
+++ src/01_pengantar/listing1-14/listing1_14.js
@@ -0,0 +1,35 @@
+// THIS IS VALID WITH var...
+
+var number = 1;
+
+// 500 lines later
+
+// var with same name redeclared is valid
+var number = 2; 
+
+var echoer = function(message) {
+    // var with argument name is valid
+    // gets overwritten
+ var message = "Local message";
+  console.log(message);
+  return message;
+}
+
+echoer("Hello there!");
+
+let number = 1;
+
+// 500 lines later
+
+// let with same name redeclared is an error
+let number = 2;
+
+
+let echoer = function(message) {
+  // Reusing function argument name as let is an error
+  let message = "Local message"; //Duplicate declaration "message"
+  console.log(message);
+  return message;
+}
+
+echoer("Hello there!");
