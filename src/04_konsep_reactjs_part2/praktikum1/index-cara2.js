5c6458405a3c25f1b573f5f7201efeda3c2b5253
AndriYss <andriyoga89@gmail.com>
2021-03-02 10:45:47 +0700

    konsep_reactjs_part2
    konsep_reactjs_part2

---
+++ src/04_konsep_reactjs_part2/praktikum1/index-cara2.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js'
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: true };
+
+    // binding ini dibutuhkan agar dapat bekerja ketika pemanggilan
+
+  }
+
+  handleClick = () => {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON - 1841720157' : 'OFF - Andri Yoga S'}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('root')
+);
---
+++ src/04_konsep_reactjs_part2/praktikum5/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+function Blog(props) {
+  const sidebar = (
+    <ul>
+      {props.posts.map((post) =>
+        <li key={post.id}>
+          {post.title}
+        </li>
+      )}
+    </ul>
+  );
+  const content = props.posts.map((post) =>
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  );
+  return (
+    <div>
+      {sidebar}
+      <hr />
+      {content}
+    </div>
+  );
+}
+
+const posts = [
+  { id: 1, title: 'Biodata Saya', content: 'NIM: 1841720157 - Nama: Andri Yoga Susila' },
+  { id: 2, title: 'Alamat Rumah', content: 'Alamat rumah saya di Ds.Sawentar RT:03 RW:04, kanigoro, Blitar' }
+];
+ReactDOM.render(
+  <Blog posts={posts} />,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
