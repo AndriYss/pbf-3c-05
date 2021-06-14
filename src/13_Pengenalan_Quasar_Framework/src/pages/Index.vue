8dd9bc2ed6db5b0896d973c9ca942e51ac3c3ad2
AndriYss <andriyoga89@gmail.com>
2021-06-14 20:26:20 +0700

    Codelab13
    Codelab13

---
+++ src/13_Pengenalan_Quasar_Framework/src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    <h3>{{ hello }}</h3>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      hello: "Halo, Nama Saya Andri Dengan NIM 1841720157"
+    }
+  }
+}
+</script>
